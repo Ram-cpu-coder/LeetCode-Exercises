c3cd0913934f9682b068b06fc5eb0423ea74c87f
Ram Kumar Dhimal <ramkumardhimal2058@gmail.com>
2024-11-01 10:41:57 +1100

    decimal to binary

--- script.js
+++ script.js
@@ -36,27 +36,48 @@
 // date can be written in its binary representation obtained by converting year, month, and day to their binary representations without any leading zeroes and writing them down in year-month-day format.
 
 // Return the binary representation of date.
-const date = "2070-02-29";
-let year = parseInt(date.split("-")[0]);
-let month = parseInt(date.split("-")[1]);
-let day = parseInt(date.split("-")[2]);
+// const date = "2070-02-29";
+// let year = parseInt(date.split("-")[0]);
+// let month = parseInt(date.split("-")[1]);
+// let day = parseInt(date.split("-")[2]);
 
-console.log(year, month, day);
+// console.log(year, month, day);
 
-const decimalToBinary = (decimal)=>{
-    let binary ='';
-    if(decimal === 0){
-            return 0;
-        }
-    while(decimal > 0){
-        const remainder = decimal % 2;
-        binary = remainder + binary; 
-        decimal = Math.floor(decimal/2);
+// const decimalToBinary = (decimal)=>{
+//     let binary ='';
+//     if(decimal === 0){
+//             return 0;
+//         }
+//     while(decimal > 0){
+//         const remainder = decimal % 2;
+//         binary = remainder + binary; 
+//         decimal = Math.floor(decimal/2);
        
-    }
-    return binary;  
-}
-console.log(`${decimalToBinary(year)}-${decimalToBinary(month)}-${decimalToBinary(day)}`);
+//     }
+//     return binary;  
+// }
+// console.log(`${decimalToBinary(year)}-${decimalToBinary(month)}-${decimalToBinary(day)}`);
 
 
 // =====================================================================================================
+
+// BINARY TO DECIMAL
+
+const binary = "11111100100-1011-1";
+let year = binary.split("-")[0];
+let month = binary.split("-")[1];
+let day = binary.split("-")[2];
+
+console.log(year, month, day);
+
+const binaryToDecimal =(inputBinary)=>{
+    let result = 0;
+    const lastIndex = inputBinary.length;
+    for(let i = 0; i < lastIndex; i++){
+        result += parseInt(inputBinary[i] * 2**(lastIndex-1-i));
+        // console.log(result);
+         
+    }
+    return result;
+}
+console.log(binaryToDecimal(year));
